2be193f97f0c6ecf339e4822f39a027c03f5eb9e
Jahanvi Saini <jahanvisaini0307@gmail.com>
2026-04-06 20:07:52 +0530

    Delete smart prep directory

--- smart prep/backend/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDashboardData, recordPractice } = require('../controllers/dashboardController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.get('/', protect, getDashboardData);
-router.post('/practice', protect, recordPractice);
-
-module.exports = router;

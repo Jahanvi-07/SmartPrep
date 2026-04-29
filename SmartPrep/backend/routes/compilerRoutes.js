a36a27e9f7d64c5a630b11e34a72d5f51696708e
Jahanvi Saini <jahanvisaini0307@gmail.com>
2026-04-30 01:05:05 +0530

    Delete SmartPrep directory

--- SmartPrep/backend/routes/compilerRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { executeCode } = require('../controllers/compilerController');
-const { protect } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.post('/execute', protect, executeCode);
-
-module.exports = router;
--- SmartPrep/backend/routes/generatorRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateQuestions } = require('../controllers/generatorController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/', protect, generateQuestions);
-
-module.exports = router;
